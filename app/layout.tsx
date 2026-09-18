d80c7086b9588960035da7e40df97a44329cf539
PenielOkoli <okolipeniel@gmail.com>
2026-09-18 10:50:44 +0100

    fix: replace dynamic Facebook pixel ID with a hardcoded value

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Script from "next/script";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
-const pixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
+const pixelId = "4630814730482514";
 
 export const metadata: Metadata = {
   title: "Ruach Store - Cordless Pressure Washer",
@@ -23,26 +23,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        {pixelId ? <Script id="fb-pixel" strategy="afterInteractive">
+        <Script id="fb-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '4630814730482514');
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,
+            'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
             fbq('track', 'PageView');
           `}
-        </Script> : null}
+        </Script>
       </head>
       <body className={dmSans.className}>
         {children}
-           {pixelId ? <noscript>
-             <img height="1" width="1" style={{ display: "none" }} src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`} alt="" />
-           </noscript> : null}
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`} alt="" />
+        </noscript>
       </body>
     </html>
   );
